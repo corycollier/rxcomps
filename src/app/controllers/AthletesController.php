85fff02f0cbd87521503f3c9362c8849523d4c55
Cory Collier <ccollier@travelclick.com>
2012-10-25 17:20:44 -0400

    adding the first iteration of the athletes section

---
+++ src/app/controllers/AthletesController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Athletes Controller
+ *
+ * This controller handles all necessary steps to create view and modify Athletes
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       File available since release 1.0.0
+ * @filesource
+ */
+
+/**
+ * Athletes Controller
+ *
+ * This controller handles all necessary steps to create view and modify Athletes
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       Class available since release 1.0.0
+ */
+
+class AthletesController
+    extends Rx_Controller_Model
+{
+    /**
+     * Specify the name of the model to be used
+     *
+     * @var string
+     */
+    protected $_modelName = 'Athlete';
+
+    /**
+     * indexAction()
+     *
+     * This is the main action for the Athletes controller
+     */
+    public function indexAction ( )
+    {
+
+    } // END function indexAction
+
+} // END class App_Controller_AthletesController
---
+++ src/app/models/Athlete.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Athletes Model
+ *
+ * This model represents individual events of the application
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  Model
+ * @copyright   Copyright (c) 2012 RxCompetition, Inc (http://www.rxcompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       File available since release 1.0.0
+ * @filesource
+ */
+
+/**
+ * Athletes Model
+ *
+ * This model represents individual events of the application
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  Model
+ * @copyright   Copyright (c) 2012 RxCompetition, Inc (http://www.rxcompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       Class available since release 1.0.0
+ */
+
+class App_Model_Athlete
+    extends Rx_Model_Abstract
+{
+
+}// END class App_Model_Athletes
+
