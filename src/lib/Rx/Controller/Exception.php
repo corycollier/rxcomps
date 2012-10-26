fcf9c814d9baad3f446dfbf64f0480c5cdc6f398
Cory Collier <ccollier@travelclick.com>
2012-10-26 13:19:55 -0400

    creating an exception to namespace errors from controllers

---
+++ src/lib/Rx/Controller/Exception.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Controller Exception
+ *
+ * This exception should provide valuable debugging information for controller
+ * error handling
+ *
+ * @category    RxCompetition
+ * @package     Rx
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       File available since release 1.0.0
+ * @filesource
+ */
+
+/**
+ * Controller Exception
+ *
+ * This exception should provide valuable debugging information for controller
+ * error handling
+ *
+ * @category    RxCompetition
+ * @package     Rx
+ * @subpackage  Controller
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       Class available since release 1.0.0
+ */
+
+class Rx_Controller_Exception
+    extends Zend_Exception
+{
+
+
+} // END class Rx_Controller_Exception
