dfdcea8a68d5ac380b4d2fb2d1e2855b52b3090d
Cory Collier <ccollier@travelclick.com>
2012-11-01 17:50:04 -0400

    adding the scale-filter to the application

---
+++ src/app/views/helpers/ScaleFilter.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Scale Filter View Helper
+ *
+ * This view helper provides a list of links that allow a user to add the scale_id
+ * to the current location, thus allowing for the filtering of current items
+ * by scale_id
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  View_Helper
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       File available since release 1.0.0
+ * @filesource
+ */
+
+/**
+ * Scale Filter View Helper
+ *
+ * This view helper provides a list of links that allow a user to add the scale_id
+ * to the current location, thus allowing for the filtering of current items
+ * by scale_id
+ *
+ * @category    RxCompetition
+ * @package     App
+ * @subpackage  View_Helper
+ * @copyright   Copyright (c) 2012 RxCompetition.com, Inc (http://www.RxCompetition.com)
+ * @license     All Rights Reserved
+ * @version     Release: 1.0.0
+ * @since       Class available since release 1.0.0
+ */
+
+class App_View_Helper_ScaleFilter
+    extends Zend_View_Helper_HtmlList
+{
+    /**
+     * _getScaleTable()
+     *
+     * Gets a new instance of the scale database table class
+     *
+     * @return App_Model_DbTable_Scale
+     */
+    protected function _getScaleTable ( )
+    {
+        return new App_Model_DbTable_Scale;
+
+    } // END function _getScaleTable
+
+    /**
+     * getScales()
+     *
+     * Gets all of the scales in the system
+     *
+     * @return Zend_Db_Table_Rowset
+     */
+    public function getScales ( )
+    {
+        $table = $this->_getScaleTable();
+
+        $scales = $table->fetchAll();
+
+        return $scales;
+
+    } // END function getScales
+
+    /**
+     * scaleFilter()
+     *
+     * Returns an html list of links to filter by scale_id
+     *
+     * @return string
+     */
+    public function scaleFilter ( )
+    {
+        $scales = $this->getScales();
+
+        $urls = array();
+
+        foreach ($scales as $scale) {
+            $urls[] = $this->view->htmlAnchor($scale->name, array(
+                'scale_id' => $scale->id,
+            ));
+        }
+
+        return $this->htmlList($urls, false, array('class' => 'actions'), false);
+
+    } // END function scaleFilter
+
+} // END class App_View_Helper_ScaleFilter
