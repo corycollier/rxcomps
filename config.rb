4975064e6ff04204da318acaad3a4a27b8b01602
Cory Collier <ccollier@travelclick.com>
2013-03-12 11:22:55 -0400

    updating styles

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'sassy-math'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "src/pub/css"
-sass_dir = "src/app/sass"
+sass_dir = "src/app/views/sass"
 images_dir = "src/pub/img"
 
 # You can select your preferred output style here (can be overridden via the command line) :nested or :expanded or :compact or :compressed:
