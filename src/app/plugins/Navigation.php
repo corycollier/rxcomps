e3ae87213fc1a480b55ad69423a1a038e06693d3
Cory Collier <ccollier@travelclick.com>
2013-08-20 13:51:10 -0400

    fixing styles a bit, and adding some links

--- src/app/plugins/Navigation.php
+++ src/app/plugins/Navigation.php
@@ -139,16 +139,16 @@ class App_Plugin_Navigation
             //         'event_id'      => $eventId,
             //     ),
             // ),
-            // array(
-            //     'label'         => 'Admin',
-            //     'controller'    => 'event-admin',
-            //     'resource'      => 'event-admin',
-            //     'privilege'     => 'edit',
-            //     'action'        => 'index',
-            //     'params' => array(
-            //         'event_id'      => $eventId,
-            //     ),
-            // ),
+            array(
+                'label'         => 'Admin',
+                'controller'    => 'admin',
+                'resource'      => 'admin',
+                'privilege'     => 'edit',
+                'action'        => 'index',
+                'params' => array(
+                    'event_id'      => $eventId,
+                ),
+            ),
         );
 
     }
